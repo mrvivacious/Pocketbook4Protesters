8163cf600311eca433bb149cddf9b31f6b59aee8
Vivek Bhookya <v.bhookya98@gmail.com>
2020-06-12 00:11:17 -0500

    Added 2 new buttons, need 2 more colors

--- lib/Data.dart
+++ lib/Data.dart
@@ -28,35 +28,43 @@ List<Planet> planets = [
   ),
   const Planet(
       id: "2",
-      name: "Bail Resources",
+      name: "Reasons For Arrests",
       description: "Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. Neptune is 17 times the mass of Earth and is slightly more massive than its near-twin Uranus, which is 15 times the mass of Earth and slightly larger than Neptune. Neptune orbits the Sun once every 164.8 years at an average distance of 30.1 astronomical units (4.50×109 km). It is named after the Roman god of the sea and has the astronomical symbol ♆, a stylised version of the god Neptune's trident",
       image: "Assets/Images/blm_color.jpg",
       picture: "https://freenaturestock.s3.us-east-1.amazonaws.com/1624.jpg",
       color: Color(0xFF653821),
       textColor: Colors.black
-
   ),
-//  const Planet(
-//      id: "3",
-//      name: "What police can/can't do",
-//      description: "The Moon is an astronomical body that orbits planet Earth, being Earth's only permanent natural satellite. It is the fifth-largest natural satellite in the Solar System, and the largest among planetary satellites relative to the size of the planet that it orbits (its primary). Following Jupiter's satellite Io, the Moon is second-densest satellite among those whose densities are known.",
-//      image: "assets/img/moon.png",
-//      picture: "https://farm5.staticflickr.com/4086/5052125139_43c31b7012.jpg",
-//      color: Colors.green
-//  ),
+  const Planet(
+      id: "3",
+      name: "Getting Arrested?",
+      description: "Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. Neptune is 17 times the mass of Earth and is slightly more massive than its near-twin Uranus, which is 15 times the mass of Earth and slightly larger than Neptune. Neptune orbits the Sun once every 164.8 years at an average distance of 30.1 astronomical units (4.50×109 km). It is named after the Roman god of the sea and has the astronomical symbol ♆, a stylised version of the god Neptune's trident",
+      image: "Assets/Images/blm_color.jpg",
+      picture: "https://freenaturestock.s3.us-east-1.amazonaws.com/1624.jpg",
+      color: Color(0xFF653821),
+      textColor: Colors.black
+  ),
   const Planet(
       id: "4",
-      name: "Protest Etiquette",
+      name: "Bail Resources",
       description: "Earth is the third planet from the Sun and the only object in the Universe known to harbor life. According to radiometric dating and other sources of evidence, Earth formed over 4 billion years ago. Earth's gravity interacts with other objects in space, especially the Sun and the Moon, Earth's only natural satellite. Earth revolves around the Sun in 365.26 days, a period known as an Earth year. During this time, Earth rotates about its axis about 366.26 times.",
       image: "Assets/Images/BLM.jpg",
       picture: "http://freenaturestock.s3.amazonaws.com/1227.jpg",
       color: Color(0xFFa16e4b),
       textColor: Colors.black
-
   ),
   const Planet(
       id: "5",
-      name: "Protect Yourself",
+      name: "Before Protesting",
+      description: "Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. Neptune is 17 times the mass of Earth and is slightly more massive than its near-twin Uranus, which is 15 times the mass of Earth and slightly larger than Neptune. Neptune orbits the Sun once every 164.8 years at an average distance of 30.1 astronomical units (4.50×109 km). It is named after the Roman god of the sea and has the astronomical symbol ♆, a stylised version of the god Neptune's trident",
+      image: "Assets/Images/blm_color.jpg",
+      picture: "https://freenaturestock.s3.us-east-1.amazonaws.com/1624.jpg",
+      color: Color(0xFFba8d57),
+      textColor: Colors.black
+  ),
+  const Planet(
+      id: "6",
+      name: "COVID19 / After Protesting",
       description: "o When you get home after protesting, "
           "take a shower to get rid of any potential germs. "
               "If you were exposed to tear gas, do not shower "
@@ -74,7 +82,7 @@ List<Planet> planets = [
       textColor: Colors.black
   ),
   const Planet(
-      id: "6",
+      id: "7",
       name: "More resources",
       description: "Mercury is the smallest and innermost planet in the Solar System. Its orbital period around the Sun of 88 days is the shortest of all the planets in the Solar System. It is named after the Roman deity Mercury, the messenger to the gods.",
       image: "Assets/Images/cool.jpg",
