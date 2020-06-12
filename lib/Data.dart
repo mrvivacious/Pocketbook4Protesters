b872002484f383c72440f31b39745680e4e71b22
Vivek Bhookya <v.bhookya98@gmail.com>
2020-06-11 23:54:43 -0500

    removed some object variables; change bg color to black

--- lib/Data.dart
+++ lib/Data.dart
@@ -5,9 +5,6 @@ import 'package:flutter/material.dart';
 class Planet {
   final String id;
   final String name;
-  final String location;
-  final String distance;
-  final String gravity;
   final String description;
   final String image;
   final String picture;
@@ -15,7 +12,7 @@ class Planet {
   final Color color;
   final Color textColor;
 
-  const Planet({this.id, this.name, this.location, this.distance, this.gravity,
+  const Planet({this.id, this.name,
     this.description, this.image, this.picture, this.color, this.textColor});
 }
 
@@ -23,9 +20,6 @@ List<Planet> planets = [
   const Planet(
       id: "1",
       name: "Know Your Rights",
-      location: "Milkyway Galaxy",
-      distance: "54.6m Km",
-      gravity: "3.711 m/s ",
       description: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war, and is often referred to as the 'Red Planet' because the reddish iron oxide prevalent on its surface gives it a reddish appearance that is distinctive among the astronomical bodies visible to the naked eye. Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the valleys, deserts, and polar ice caps of Earth.",
       image: "Assets/Images/a_better_world.jpg",
       picture: "https://raw.githubusercontent.com/sergiandreplace/planets-flutter/master/assets/img/mars.png",
@@ -35,9 +29,6 @@ List<Planet> planets = [
   const Planet(
       id: "2",
       name: "Bail Resources",
-      location: "Milkyway Galaxy",
-      distance: "54.6m Km",
-      gravity: "3.711 m/s ",
       description: "Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. Neptune is 17 times the mass of Earth and is slightly more massive than its near-twin Uranus, which is 15 times the mass of Earth and slightly larger than Neptune. Neptune orbits the Sun once every 164.8 years at an average distance of 30.1 astronomical units (4.50×109 km). It is named after the Roman god of the sea and has the astronomical symbol ♆, a stylised version of the god Neptune's trident",
       image: "Assets/Images/blm_color.jpg",
       picture: "https://freenaturestock.s3.us-east-1.amazonaws.com/1624.jpg",
@@ -48,9 +39,6 @@ List<Planet> planets = [
 //  const Planet(
 //      id: "3",
 //      name: "What police can/can't do",
-//      location: "Milkyway Galaxy",
-//      distance: "54.6m Km",
-//      gravity: "3.711 m/s ",
 //      description: "The Moon is an astronomical body that orbits planet Earth, being Earth's only permanent natural satellite. It is the fifth-largest natural satellite in the Solar System, and the largest among planetary satellites relative to the size of the planet that it orbits (its primary). Following Jupiter's satellite Io, the Moon is second-densest satellite among those whose densities are known.",
 //      image: "assets/img/moon.png",
 //      picture: "https://farm5.staticflickr.com/4086/5052125139_43c31b7012.jpg",
@@ -59,9 +47,6 @@ List<Planet> planets = [
   const Planet(
       id: "4",
       name: "Protest Etiquette",
-      location: "Milkyway Galaxy",
-      distance: "54.6m Km",
-      gravity: "3.711 m/s ",
       description: "Earth is the third planet from the Sun and the only object in the Universe known to harbor life. According to radiometric dating and other sources of evidence, Earth formed over 4 billion years ago. Earth's gravity interacts with other objects in space, especially the Sun and the Moon, Earth's only natural satellite. Earth revolves around the Sun in 365.26 days, a period known as an Earth year. During this time, Earth rotates about its axis about 366.26 times.",
       image: "Assets/Images/BLM.jpg",
       picture: "http://freenaturestock.s3.amazonaws.com/1227.jpg",
@@ -72,9 +57,6 @@ List<Planet> planets = [
   const Planet(
       id: "5",
       name: "Protect Yourself",
-      location: "Milkyway Galaxy",
-      distance: "54.6m Km",
-      gravity: "3.711 m/s ",
       description: "o When you get home after protesting, "
           "take a shower to get rid of any potential germs. "
               "If you were exposed to tear gas, do not shower "
@@ -94,9 +76,6 @@ List<Planet> planets = [
   const Planet(
       id: "6",
       name: "More resources",
-      location: "Milkyway Galaxy",
-      distance: "54.6m Km",
-      gravity: "3.711 m/s ",
       description: "Mercury is the smallest and innermost planet in the Solar System. Its orbital period around the Sun of 88 days is the shortest of all the planets in the Solar System. It is named after the Roman deity Mercury, the messenger to the gods.",
       image: "Assets/Images/cool.jpg",
       picture: "http://freenaturestock.s3.amazonaws.com/1187.jpg",
